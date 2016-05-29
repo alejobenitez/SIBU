2174a8b134d68601b091c5f681b5620cc9f31039
alejobenitez <alejandro_benitez@hotmail.com>
2016-05-28 20:40:06 -0500

    archivos

---
+++ Presentacion/Inicio.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+    <meta charset="utf-8" />
+    <title> SIBU </title>
+    <?php include 'header.html'; ?>
+
+</head>
+<body>
+<header class="row-eq-height center">
+
+    <div class="col-xs-4">
+        <a href="login.php" class="btn btn-default vertical-center">Ingresar</a>
+    </div>
+</header>
+<main>
+    <p>
+        Con este proyecto queremos mostrar como la información puede ser compartida por medio de una<br>
+        plataforma informática de consultas de libros encontrados en las bibliotecas de las universidades<br>
+        de la ciudad de Medellín que permita brindar a los usuarios el manejo ágil y oportuno de la misma.<br>
+    </p>
+</main>
+<footer class="about">
+    <?php include 'footer.html'; ?>
+</footer>
+</body>
+</html>
