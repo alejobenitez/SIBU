3bb532a0035bf0ee633b45ed62e4fdf2b01157dd
alejobenitez <alejandro_benitez@hotmail.com>
2016-05-28 20:41:04 -0500

    archivos

---
+++ Presentacion/restablecer.php
@@ -0,0 +1,68 @@
+<?php
+include ("../Negocio/ConexionMySQL.php");
+
+if(isset($_GET['action']))
+{          
+    if($_GET['action']=="reset")
+    {
+        $encrypt = mysqli_real_escape_string($conex,$_GET['encrypt']);
+        $query = "SELECT CORREO FROM usuario where md5(90*13+CORREO)='".$encrypt."'";
+        $result = mysqli_query($conex,$query);
+        $Results = mysqli_fetch_array($result);
+        if(count($Results)>=1)
+        {
+        }
+        else
+        {
+            echo 'Clave invalida por favor intente de nuevo . <a href="..Presentacion/recordar.php">recordar contrasena?</a>';
+        }
+    }
+}
+elseif(isset($_POST['action']))
+{
+	$encrypt      = mysqli_real_escape_string($conex,$_POST['action']);
+    $password     = mysqli_real_escape_string($conex,$_POST['password']);
+    $query = "SELECT CORREO FROM usuario where md5(90*13+CORREO)='".$encrypt."'";
+    $result = mysqli_query($conex,$query);
+    $Results = mysqli_fetch_array($result);
+    if(count($Results)>=1)
+    {
+        $query = "update usuario set psw_Usuario='".md5($password)."' where CORREO='".$Results['CORREO']."'";
+        mysqli_query($conex,$query);
+		echo 'La contrase&ntildea se cambio con exito.'; 
+		header( "refresh:3; url=http://proyectotaller.sytes.net/proyectotaller/proyectoHenry/view/login.html" );
+		exit; 
+        //echo "Su contraseña fue cambiada correctamente!!";
+		//header( "refresh:3; url=http://proyectotaller.sytes.net/proyectotaller/proyectoHenry/view/login.html" );
+    }
+    else
+    {
+         echo 'Clave invalida por favor intente de nuevo . <a href="..Presentacion/recordar.php">recordar contrasena?</a>';
+    }
+}
+else
+{
+   
+}
+
+?>
+<head>
+<link rel="stylesheet" type="text/css" href="../public/css/estiloMenu.css" />
+<link rel="stylesheet" type="text/css" href="../public/css/estiloCarga.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>RESTABLECER CONTRASEÑA | SIBU</title>\
+    <?php include 'header.html'; ?>
+</head>
+<body>
+<h1> RESTABLECER CONTRASEÑA </h1>
+  <form action="../Presentacion/restablecer.php" method="post" class="container" id="reset" >
+    <p><input id="password" name="password" type="password" placeholder="Escriba la nueva contrasena">
+    <p><input id="password2" name="password2" type="password" placeholder="Escriba de nuevo su contrasena">
+    <input name="action" type="hidden" value="<?php echo $encrypt;?>" /></p>
+    <p><input type="submit" value="Restablecer contraseña"/></p>
+  </form>
+
+<footer class="about">
+    <?php include 'footer.html'; ?>
+</footer>
+</body>
